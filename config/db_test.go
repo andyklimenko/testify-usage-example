1e6358f44e9fd24d1861a12fa1a3a140aa4c244e
Andrii Klymenko <andrii@tradevest.ai>
2023-11-23 14:29:59 +0100

    added a test with no testify used for comparison

--- config/db_test.go
+++ config/db_test.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"errors"
 	"os"
 	"testing"
 
@@ -12,11 +13,36 @@ func TestDBLoad(t *testing.T) {
 	t.Parallel()
 
 	var db DB
-	assert.EqualError(t, db.Load("test.db"), ErrNoDbDSN.Error())
+	assert.ErrorIs(t, db.Load("test.db"), ErrNoDbDSN)
 
 	require.NoError(t, os.Setenv("TEST_DB_DSN", "localhost"))
 	assert.NoError(t, db.Load("test.db"))
 
 	assert.Equal(t, "postgres", db.Driver)
 	assert.Equal(t, "localhost", db.DSN)
+}
+
+func TestDBLoadOldWay(t *testing.T) {
+	t.Parallel()
+
+	var db DB
+	if err := db.Load("test.db"); errors.Is(err, ErrNoDbDSN) {
+		t.Fatal(err)
+	}
+
+	if err := os.Setenv("TEST_DB_DSN", "localhost"); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := db.Load("test.db"); err != nil {
+		t.Fatal(err)
+	}
+
+	if db.Driver != "postgres" {
+		t.Errorf("%s was expected byt %s got", "postgres", db.Driver)
+	}
+
+	if db.DSN != "localhost" {
+		t.Errorf("%s was expected byt %s got", "localhost", db.DSN)
+	}
 }
